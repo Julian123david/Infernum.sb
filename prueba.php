29a6ce72b20eb11d3b4fbf4515631a0982bf4584
unknown <sanchezjclaret7@gmail.com>
2022-09-08 16:54:55 -0500

    Avances

--- prueba.php
+++ prueba.php
@@ -27,42 +27,6 @@
 	</nav>
 </header>
 
-<div class="slide">
-			<div class="slide-inner">
-				<input class="slide-open" type="radio" id="slide-1" 
-			 	     name="slide" aria-hidden="true" hidden="" checked="checked">
-				<div class="slide-item">
-					<img src="https://www.migueltroyano.com/wp-content/uploads/2020/10/embeddingApi_7.jpg">
-				</div>
-				<input class="slide-open" type="radio" id="slide-2" 
-			 	     name="slide" aria-hidden="true" hidden="">
-				<div class="slide-item">
-					<img src="https://www.migueltroyano.com/wp-content/uploads/2020/09/postgres_copy.png">
-				</div>
-				<input class="slide-open" type="radio" id="slide-3" 
-			 	     name="slide" aria-hidden="true" hidden="">
-				<div class="slide-item">
-					<img src="https://www.migueltroyano.com/wp-content/uploads/2020/09/excel_guardar_como_csv.jpg">
-				</div>
-				<label for="slide-3" class="slide-control prev control-1">‹</label>
-				<label for="slide-2" class="slide-control next control-1">›</label>
-				<label for="slide-1" class="slide-control prev control-2">‹</label>
-				<label for="slide-3" class="slide-control next control-2">›</label>
-				<label for="slide-2" class="slide-control prev control-3">‹</label>
-				<label for="slide-1" class="slide-control next control-3">›</label>
-				<ol class="slide-indicador">
-					<li>
-						<label for="slide-1" class="slide-circulo">•</label>
-					</li>
-					<li>
-						<label for="slide-2" class="slide-circulo">•</label>
-					</li>
-					<li>
-						<label for="slide-3" class="slide-circulo">•</label>
-					</li>
-				</ol>
-			</div>
-		</div>
 
 <center>
     <h3>Nuestros Productos</h3>
